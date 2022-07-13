5749c0d26a7fd41c9691f1e8c2829f17a46a7541
Joseph David <techiejossy@gmail.com>
2022-07-13 23:08:20 +0100

    feat: Add base64 encoded metadata of the NFT

--- packages/hardhat/contracts/TxbiTix.sol
+++ packages/hardhat/contracts/TxbiTix.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract TxbiTix is ERC721URIStorage {
     // counter to keep track of the current token ID
@@ -25,7 +27,45 @@ contract TxbiTix is ERC721URIStorage {
     // Token mint funciton
     function mint() public {
         require(availableTickets > 0, "tickets outsold");
+
+        string[3] memory svg;
+        svg[
+            0
+        ] = '<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><text y="50">';
+        svg[1] = Strings.toString(currentId.current());
+        svg[2] = "</text></svg>";
+
+        // Interpolate and encode svg image
+        string memory image = string(abi.encodePacked(svg[0], svg[1], svg[2]));
+        string memory encodedImage = Base64.encode(bytes(image));
+        console.log(encodedImage);
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{ "name": "TxbiTix #',
+                        Strings.toString(currentId.current()),
+                        '", "description": "A NFT-powered ticketing system", ',
+                        '"traits": [{ "trait_type": "Checked In", "value": "true" }, { "trait_type": "Purchased", "value": "true" }], ',
+                        '"image": "data:image/svg+xml;base64,',
+                        encodedImage,
+                        '" }'
+                    )
+                )
+            )
+        );
+
+        string memory tokenURI = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        console.log(tokenURI);
+
+        console.log(tokenURI);
+
         _safeMint(msg.sender, currentId.current());
+        _setTokenURI(currentId.current(), tokenURI);
+
         currentId.increment();
         availableTickets = availableTickets - 1;
     }
